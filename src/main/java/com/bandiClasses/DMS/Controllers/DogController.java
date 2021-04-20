3e7096abe138096ae409e2e8b77d0e670663d173
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2021-04-20 13:54:56 -0500

    Update DogController.java

--- src/main/java/com/bandiClasses/DMS/Controllers/DogController.java
+++ src/main/java/com/bandiClasses/DMS/Controllers/DogController.java
@@ -1,14 +1,38 @@
 package com.bandiClasses.DMS.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bandiClasses.DMS.Models.Dog;
+import com.bandiClasses.DMS.repository.DogRepository;
 
 @Controller
 public class DogController {
-	@RequestMapping("dogHome")
-	public String home() {
-		return "home";
-	}
 	
+	ModelAndView mv=new ModelAndView();
+	@Autowired
+	DogRepository dogRepo;
+
+	/*
+	 * @RequestMapping("dogHome") public String home() { return "home"; }
+	 */
+	 @RequestMapping("dogHome")
+	public ModelAndView home() {
+		mv.setViewName("home");
+		return mv;
+	}
+	 @RequestMapping("add")
+	 public ModelAndView add() {
+		 mv.setViewName("addNewDog.html");
+		 return mv;
+	 }
+	 @RequestMapping("addNewDog")
+	 public ModelAndView addNewDog(Dog dog) {
+		 dogRepo.save(dog);
+		 mv.setViewName("home");
+		 return mv;
+	 }
 
 }
